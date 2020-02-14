f418c2daab7e00995d4f9708243bd9ed0670ee93
root <root@qmaster02.cluster>
2020-02-14 06:28:01 -0500

    Fixed bug where it was parsing out LSF's header and tail, but in SLURM it is just removing valid sequences

--- FindFungi-0.23.3.sh
+++ FindFungi-0.23.3.sh
@@ -166,7 +166,7 @@ echo "Starting : BLAST against the genome : $(date)"
 for d in $Dir/Processing/ReadNames_bsub.*.fsa; do
     File=$(basename $d)
     Taxid=$(echo $File | awk -F '.' '{print $2}')
-    tail -n +31 $d | head -n -6 > $Dir/Processing/ReadNames.$Taxid.fsa
+    cat $d > $Dir/Processing/ReadNames.$Taxid.fsa
     # bsub -K -q C blastn -task megablast -query $Dir/Processing/ReadNames.$Taxid.fsa -db $BLAST_DB_Dir/Taxid-$Taxid -out $Dir/Results/BLAST_Processing/BLAST.$Taxid -evalue 1E-20 -num_threads 30 -outfmt 6 &
     #blastn -task megablast -query $Dir/Processing/ReadNames.$Taxid.fsa -db $BLAST_DB_Dir/Taxid-$Taxid -out $Dir/Results/BLAST_Processing/BLAST.$Taxid -evalue 1E-20 -num_threads 20 -outfmt 6 &
     sbatch -W --job-name=${Taxid} --cpus-per-task=20 --wrap="blastn -task megablast -query ${Dir}/Processing/ReadNames.${Taxid}.fsa -db ${BLAST_DB_Dir}/Taxid-${Taxid} -out ${Dir}/Results/BLAST_Processing/BLAST.${Taxid} -evalue 1E-20 -num_threads 20 -outfmt 6" & 
